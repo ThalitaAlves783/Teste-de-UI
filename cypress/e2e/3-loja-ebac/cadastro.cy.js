147735bf786a0b86e511e69e5b6a7717eb83ebc1
Thalita Alves <thalitaalves783@outlook.com>
2024-06-16 17:16:46 -0300

    usando biblioteca faker.js para massa de dados

---
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -0,0 +1,18 @@
+//<reference types="cypress"/>
+
+import { faker } from '@faker-js/faker';
+
+context('Funcionalidade Cadastro', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    it('Deve completar o cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('325212451')
+        cy.get(':nth-child(4) > .button').click()
+    });
+
+});
+
